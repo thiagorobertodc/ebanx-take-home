8851331b2664f272cc9e90fc2563185c3b0412f6
Thiago Roberto <thiago@nursa.com>
2024-12-16 16:59:09 -0300

    fix: typo in balance controller

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -3,14 +3,15 @@ const { balanceService } = require("../services");
 
 const getBalance = catchAsync(async (req, res) => {
   const accountId = parseInt(req.query.account_id);
+  console.log(accountId);
   if (!accountId) {
     return res.status(400).send("missing account id");
   }
   try {
-    const balance = await balanceService.getBalanceByAccountId(account_id);
+    const balance = await balanceService.getBalanceByAccountId(accountId);
     return res.status(200).send(`${balance}`);
   } catch (error) {
-    return res.status(404).send("0"); // Account not found
+    return res.status(404).send("0");
   }
 });
 
